b715cf9b7e8892c717c9f25313113969d6c9a12f
rahulpoptani <rahulpoptani90@gmail.com>
2024-06-29 12:03:33 +0530

    oracle initial setup sql

---
+++ Oracle_Initial_Setup.sql
@@ -0,0 +1,9 @@
+create user rahul identified by rahul;
+grant all privileges to rahul;
+grant dba to rahul;
+grant connect to rahul;
+
+create database rahul;
+create schema rahul;
+
+select 1 from dual;
